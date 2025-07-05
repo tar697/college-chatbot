a7451d036dee9daf84299eb920522616d7a8b2df
Taruna aggarwal <taruna.aggarwal.27@gmail.com>
2025-07-05 16:36:53 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1N0WZL8H_BBWyxK5MkXP3Zyt7iWSYwua9
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
